c05f0e4be6d85d4be176dae38b2292fcb08d513d
Damjan Dabo <damjan.dabo@infinum.hr>
2019-03-22 15:11:10 +0100

    Created Bugsnatch singleton.

---
+++ iOS-Bugsnatch/Classes/Bugsnatch.swift
@@ -0,0 +1,15 @@
+//
+//  Bugsnatch.swift
+//  iOS-Bugsnatch
+//
+//  Created by Damjan Dabo on 22/03/2019.
+//
+
+import Foundation
+
+public final class Bugsnatch {
+
+    private init() {}
+
+    public static let shared = Bugsnatch()
+}
