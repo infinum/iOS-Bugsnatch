630bde54d12fe57833b6d51d5d9ae3ee150f9671
Damjan Dabo <damjan.dabo@infinum.hr>
2019-05-16 14:28:49 +0200

    Changed the color of the status bar in the example project.

--- Example/iOS-Bugsnatch/ViewController.swift
+++ Example/iOS-Bugsnatch/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         let productiveConfig = ProductiveConfig(organizationId: "1-infinum", projectId: 1116)
         let bugsnatchConfig = BugsnatchConfig(
             trigger: ScreenshotTrigger(),
@@ -22,9 +21,8 @@ class ViewController: UIViewController {
         Bugsnatch.shared.setup(config: bugsnatchConfig)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
     }
 }
 
